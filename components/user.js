bbbe205a583a3ff4f7065a1155cb9b7472fe420d
ritesh.rajput <ritesh.rajput@supersourcing.com>
2025-08-25 23:18:52 +0530

    data add

--- components/user.js
+++ components/user.js
@@ -4,5 +4,5 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   number: { type: Number, required: true }
 }, { timestamps: true });
-
+console.log("data")
 module.exports = mongoose.model("User", userSchema);
